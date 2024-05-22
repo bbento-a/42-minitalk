a970d704d17448ada5de63867c527828203d9c9f
bbento-a <bbento-a@c2r2s7.42lisboa.com>
2024-05-22 13:59:57 +0100

    changes in bits

--- src/client.c
+++ src/client.c
@@ -6,26 +6,37 @@
 /*   By: bbento-a <bbento-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 15:59:50 by bbento-a          #+#    #+#             */
-/*   Updated: 2024/05/20 12:39:14 by bbento-a         ###   ########.fr       */
+/*   Updated: 2024/05/22 13:38:36 by bbento-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-//  Function to convert bits to a char
-char    bits_tochar(char *bits)
-{
-    
-}
-
 void    send_msg(int pid, char *msg)
 {
+    static int bits;
     int i;
 
+    i = 0;
+    bits = 7;
     while (msg[i] != '\0')
     {
-        bit__tochar();
-        ft_addchar(msg, msg[i]);
+        while (bits >= 0)
+        {
+            if (msg[i] << bits & 1)
+            {
+                printf("%d", msg[i] >> bits);
+                kill(pid, SIGUSR1);
+            }
+            else if (msg[i] << bits & 0)
+            {
+                printf("%d", msg[i] >> bits);
+                kill(pid, SIGUSR2);
+            }
+            bits--;
+        }
+        bits = 7;
+        i++;
     }
 }
 
@@ -34,12 +45,12 @@ int main(int argc, char **argv)
     int pid;
     int i;
 
-    i = -1;
     if (argc != 3 || !argv[1][0] || !argv[2][0])
         error_occurrence();
+    i = -1;
+    pid = ft_atoi(argv[1]);
     while (argv[1][++i])
         if (!ft_isdigit(argv[1][i]))
             error_occurrence();
-    
-    send_msg(getpid, argv[2]);
+    send_msg(pid, argv[2]);
 }
