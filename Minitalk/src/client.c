eca1815c050506f3e7d0c8a0b6e745036e889b69
bbento-a <bbento-a@c1r19s1.42lisboa.com>
2024-05-27 15:44:04 +0100

    Finished printing function

--- Minitalk/src/client.c
+++ Minitalk/src/client.c
@@ -6,30 +6,11 @@
 /*   By: bbento-a <bbento-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 15:59:50 by bbento-a          #+#    #+#             */
-/*   Updated: 2024/05/22 13:38:36 by bbento-a         ###   ########.fr       */
+/*   Updated: 2024/05/27 15:39:47 by bbento-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-//  Sends the length of the message the client is sending to the server
-
-// void    send_msg_len(int pid, char *msg)
-// {
-//     int len;
-//     int bits;
-
-//     len = 0;
-//     bits = 7;
-//     while(msg[len] != '\0')
-//         len++;
-//     while (bits >= 0)
-//     {
-//         if ((len >> bits) & 1)
-//             kill(pid, SIGUSR2);
-//         bits--;
-//     }
-// }
 
 void    send_msg(int pid, char *msg)
 {
@@ -37,20 +18,19 @@ void    send_msg(int pid, char *msg)
     int i;
 
     i = 0;
-    bits = 8;
-    // send_msg_len(pid, msg);
+    bits = 0;
     while (msg[i] != '\0')
     {
-        while (bits > 0)
+        while (bits < 8)
         {
             if ((msg[i] >> bits) & 1)
                 kill(pid, SIGUSR1);
             else
                 kill(pid, SIGUSR2);
-            usleep(50);
-            bits--;
+            usleep(100);
+            bits++;
         }
-        bits = 8;
+        bits = 0;
         i++;
     }
 }
@@ -60,7 +40,7 @@ int main(int argc, char **argv)
     int pid;
     int i;
 
-    if (argc != 3 || !argv[1][0] || !argv[2][0])
+    if (argc != 3)
         error_occurrence();
     i = -1;
     while (argv[1][++i])
@@ -69,4 +49,5 @@ int main(int argc, char **argv)
     pid = ft_atoi(argv[1]);
 
     send_msg(pid, argv[2]);
+    send_msg(pid, "\n");
 }
--- Minitalk/src/server.c
+++ Minitalk/src/server.c
@@ -6,50 +6,24 @@
 /*   By: bbento-a <bbento-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 16:01:04 by bbento-a          #+#    #+#             */
-/*   Updated: 2024/05/22 12:17:11 by bbento-a         ###   ########.fr       */
+/*   Updated: 2024/05/27 15:42:09 by bbento-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// 1 global variable used in server file for
-// int gmsg_len;
-
-// void   get_msg_len(int signal)
-// {
-//     static int  len;
-//     static int  bits;
-
-//     bits = 0;
-//     if (signal == 1)
-//         len = 1 << bits;
-//     if (bits++ == 7)
-//     {
-//         gmsg_len = len;
-//         len = 0;
-//         bits = 0;
-//         ft_printf("lenght: %i\n", gmsg_len);
-//     }
-// }
-
 void get_bit(int signal)
 {
-    static int bits = 0; // bit shifter posistion
-    static int c = 0;    // bit storage
-    // static int i;
-    // static char *msg;
+    static int  bits; // bit shifter posistion
+    static char c;    // bit storage
 
-    // i = 0;
-    // msg = 0;
     if (signal == SIGUSR1)
         c |= (1 << bits);
     else if (signal == SIGUSR2)
-        c &= (0 << bits);
-    bits++;
-    if (bits == 8)
+        c &= ~(1 << bits);
+    if (++bits == 8)
     {
         write(1, &c, 1);
-        // ft_addchar(msg, c);
         bits = 0;
         c = 0;
     }
